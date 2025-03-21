fda8becca8aab8108cec3b46ae8c65648071baae
KangJaeSeong <wotjd0608@naver.com>
2025-03-21 18:02:31 +0900

    으어어ㅓㅇ수정

--- src/main/java/com/mbcit/vivere/controller/BookController.java
+++ src/main/java/com/mbcit/vivere/controller/BookController.java
@@ -1,11 +1,40 @@
 package com.mbcit.vivere.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mbcit.vivere.service.ConcertService;
+import com.mbcit.vivere.vo.ConcertTimeVO;
+import com.mbcit.vivere.vo.ConcertVO;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class BookController {
+	
+	@Autowired
+	private ConcertService concertService;
+	
+	@RequestMapping("/book")
+	public String book(HttpServletRequest request, Model model) {
+		System.out.println("BookController 컨트롤러의 book() 메소드 실행");
+		
+		int concertId = 1; // Test
+		
+		ConcertVO concertVO = concertService.getConcertById(concertId);
+		List<ConcertTimeVO> conTimes = concertService.getFutureConcertTimes(concertId);
+		
+		model.addAttribute("concertVO", concertVO);
+		model.addAttribute("conTimes", conTimes);
+		
+		return "/book";
+	}
 
 }
--- src/main/java/com/mbcit/vivere/dao/ConcertDAO.java
+++ src/main/java/com/mbcit/vivere/dao/ConcertDAO.java
@@ -1,8 +1,16 @@
 package com.mbcit.vivere.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.mbcit.vivere.vo.ConcertTimeVO;
+import com.mbcit.vivere.vo.ConcertVO;
 
 @Mapper
 public interface ConcertDAO {
+	
+	ConcertVO getConcertById(int id);
+	List<ConcertTimeVO> getConcertTimes(int id);
 
 }
